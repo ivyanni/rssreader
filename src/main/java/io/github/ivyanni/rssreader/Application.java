4022c1cba6d21ad0b634352a33dc04a4b3b7a213
ivyanni <sproot96@gmail.com>
2019-02-23 11:47:37 +0300

    Initial commit

---
+++ src/main/java/io/github/ivyanni/rssreader/Application.java
@@ -0,0 +1,10 @@
+package io.github.ivyanni.rssreader;
+
+/**
+ * @author Ilia Vianni on 23.02.2019.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
